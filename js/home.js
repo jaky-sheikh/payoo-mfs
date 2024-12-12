e790e5010e7b976283601b392b2ef0795dc35095
jaky-sheikh <jaky@eu4m.eu>
2024-12-12 19:54:49 +0600

    new balance add

--- js/home.js
+++ js/home.js
@@ -3,5 +3,21 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
 
     const addMoneyInput = document.getElementById('input-add-money').value;
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(addMoneyInput, pinNumberInput);
+    // console.log(addMoneyInput, pinNumberInput);
+    if (pinNumberInput === '1234') {
+        console.log('adding money to your account');
+
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance)
+
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else {
+        alert('Failed to add money. Please try again!')
+    }
 })
